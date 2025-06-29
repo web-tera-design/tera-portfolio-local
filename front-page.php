6029d3ef43ea05688733c16ff2369ffb143d2246
テラ <terakawayusuke@gmail.com>
2025-06-29 21:51:06 +0900

    MV調整しなおす前

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<main class="l-main">
+    <section class="p-index-mv">
+        <div class="p-index-mv__inner l-section__inner">
+            <div class="p-index-mv__content">
+                <p class="p-index-mv__label">Web coder</p>
+                <h2 class="p-index-mv__title">Tera’s portfolio</h2>
+                <div class="p-index-mv__copy">
+                    <p class="p-index-mv__lead">納期を破るくらいなら、寝ません。</p>
+                    <p class="p-index-mv__sub">ご依頼に、全力で真摯に取り組みます。</p>
+                </div>
+            </div>
+            <div class="p-index__image-wrapper">
+                <div class="p-index-mv__image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/img/mv-image-pc.webp" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
